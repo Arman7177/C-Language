300a4026e23e1639be20ceb0b5180ed938acb498
Arman7177 <armanabrahamyan33@gmail.com>
2025-03-19 12:38:05 +0400

    Sym.c

---
+++ Sym.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+         char ch;
+
+         printf("Please enter the Symbol: \n");
+         scanf("%c", &ch);
+
+         printf("The numberic value of %c = %d \n",ch,ch);
+
+         return 0;
+
+}
+
