834236406347254251a76d0ce1a5385ed893e59e
Arman7177 <armanabrahamyan33@gmail.com>
2025-03-19 12:35:29 +0400

    Num.c

---
+++ Num.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+        unsigned int num;
+
+        printf("Enter positive number: \n");
+        scanf("%u",&num);
+
+        printf("You entered %u \n",num);
+
+        return 0;
+
+}
+
+
+       
